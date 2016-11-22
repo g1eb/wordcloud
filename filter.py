230eef858d711e00a6569d98998962b27e9d96d5
Gleb <git@gleb.nl>
2016-11-22 14:32:25 +0100

    Initial commit.

---
+++ filter.py
@@ -0,0 +1,146 @@
+#!/usr/bin/python
+
+import pandas as pd
+import json, getopt, os, sys, re
+
+
+## Main
+def main(argv):
+    inputfile, noisefile, outputfile, length = get_inputfile(argv)
+    data, noise = parseInput(inputfile, noisefile)
+    data = getWordCounts(data, noise, length)
+    dumpOutput(data, outputfile)
+
+    print '\n --- All done! ---- \n'
+    sys.exit()
+
+
+## Process the arguments and return the settings
+def get_inputfile(argv):
+    err_msg = ('\n\n\n'
+               'Please provide a json input file:\n' 
+               '\n\nOptional arguments are:\n'
+               '-h, --help\t\tDisplay this message and exit\n'
+               '-l, --length\t\tPreferred output length\n'
+               '-n, --noise\t\tText file with noise terms to be filtered out\n'
+               '-o, --output\t\tPrefix for output filename\n'
+               '-i, --input\t\tJSON input file\n'
+               'Example usage could be:\n'
+               '$ ./filter.py -i data.json -o filtered_data.json\n\n')
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'l:n:o:i:hv', ['help', 'length', 'input', 'noise', 'output='])
+    except getopt.GetoptError as err:
+        print err_msg; sys.exit(2)
+
+    length = None
+    inputfile = ''
+    noisefile = ''
+    outputfile = ''
+
+    for o, a in opts:
+        if o in ('-h', '--help'):
+            print err_msg; sys.exit()
+        elif o in ("-l", "--length"):
+            length = int(float(a))
+        elif o in ("-n", "--noise"):
+            noisefile = a
+        elif o in ("-o", "--output"):
+            outputfile = a
+        elif o in ("-i", "--input"):
+            inputfile = a
+
+    #if not eventsfile or not entitiesfile or not timesfile or not opinionsfile: print err_msg; sys.exit()
+    if not length: print '..... Output length not specified, using default: 1000 or max length'; length = 1000
+    if not noisefile: print '..... Noise text file not specified';
+    if not outputfile: print '..... Output prefix not specified, using default: output.json'; outputfile = 'output'
+    if not inputfile: print '..... Input file not specified, using default: data.json'; inputfile = 'data.json'
+
+    return [inputfile, noisefile, outputfile, length]
+
+
+## Parse input files
+def parseInput(inputfile, noisefile):
+    data = None
+
+    try:
+        print '....... Reading data file'
+        print '                .... hold on ....'
+        data = pd.read_json(inputfile)
+    except IOError, e:
+        print e; sys.exit()
+
+    if noisefile:
+        with open(noisefile) as f:
+            noise = json.load(f)
+    else:
+        noise = []
+
+    return data, noise
+
+
+## Filter function used to filter out noise from input data
+def allowed(value, noise):
+
+    if not isinstance(value, basestring):
+        return False
+
+    value = value.lower()
+
+    if len(value) == 1:
+        return False
+
+    if value.isdigit():
+        return False
+
+    if value in noise:
+        return False
+
+    if re.match(r'^https?://.*$', value):
+        return False
+
+    return True
+
+
+## Translate values from one range to another
+def translate(value, leftMin, leftMax, rightMin, rightMax):
+    # Figure out how 'wide' each range is
+    leftSpan = leftMax - leftMin
+    rightSpan = rightMax - rightMin
+
+    # Convert the left range into a 0-1 range (float)
+    valueScaled = float(value - leftMin) / float(leftSpan)
+
+    # Convert the 0-1 range into a value in the right range.
+    return rightMin + (valueScaled * rightSpan)
+
+
+## Get word counts
+def getWordCounts(data, noise, length):
+    print '.......... Pandas are working their magic now'
+    print '                            ... might take a while .......'
+
+    df = pd.DataFrame(columns=['text', 'size'])
+    df['text'] = [re.sub(r'\W+$', '', word) for words in data['text'].str.split() for word in words if allowed(word, noise)]
+    df['text'] = df['text'].str.lower()
+    df['size'] = df.groupby('text')['text'].transform('count')
+    df = df.dropna().drop_duplicates()
+    df = df.sort_values(by='size', ascending=[0])
+
+    max_size = df.iloc[0]['size']
+    min_size = df.iloc[length if length < len(df.values) else len(df.values)-1]['size']
+    
+    return [{'text': x[0], 'size': translate(x[1], min_size, max_size, 10, 100)} for x in df.values[:length]]
+
+
+## Dump aggregated data
+def dumpOutput(data, outputfile):
+
+    with open(outputfile + '.json', 'w') as outfile:
+        json.dump(data, outfile)
+
+    print '...... Data dumped into ' + outputfile + '.json'
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
---
+++ src/js/cloud.js
@@ -0,0 +1,71 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function () {
+
+  //var colors = d3.scale.quantize()
+  //  .range(colorbrewer.RdYlGn[11]);
+  var colors = d3.scale.category20();
+
+  var width = 960;
+  var height = 500;
+
+  // Get data from server
+  d3.json('output.json', function(error, data) {
+    if (error) { throw error; }
+    setup(data);
+  });
+
+  /**
+   * Setup how to draw word cloud
+   * @param data List of objects with words and sizes
+   */
+  var setup = function (data) {
+    colors.domain([d3.max(data, function(d) {
+      return d.size;
+    }), 0]);
+
+    var layout = d3.layout.cloud()
+      .size([width, height])
+      .padding(5)
+      .rotate(function() {
+        return ~~(Math.random() * 2) * 90;
+      })
+      .font('Impact')
+      .fontSize(function(d) {
+        return d.size;
+      })
+      .on('end', draw);
+    layout.words(data);
+    layout.start();
+  };
+
+  /**
+   * Draw function to actually draw the cloud
+   * @param words Array of words to draw in a cloud
+   */
+  var draw = function (words) {
+    d3.select('#cloud')
+      .append('svg')
+      .attr('preserveAspectRatio', 'xMinYMin meet')
+      .attr('viewBox', '0 0 960 500')
+      .attr('class', 'svg')
+      .append('g')
+        .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')')
+      .selectAll('text')
+        .data(words)
+      .enter().append('text')
+        .style('font-size', function(d) {
+          return d.size + 'px';
+        })
+        .style('font-family', 'Impact')
+        .style('fill', function(d) {
+          return colors(d.size);
+        })
+        .attr('text-anchor', 'middle')
+        .attr('transform', function(d) {
+          return 'translate(' + [d.x, d.y] + ')rotate(' + d.rotate + ')';
+        })
+        .text(function(d) { return d.text; });
+  };
+
+});
